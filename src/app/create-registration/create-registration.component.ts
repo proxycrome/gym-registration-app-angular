7da5d72584f8ee83e9b3528edd933382c5bce666
proxycrome <e.v.ezeonwuka@gmail.com>
2023-08-14 15:41:59 +0100

    first commit

---
+++ src/app/create-registration/create-registration.component.ts
@@ -0,0 +1,136 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { NgToastService } from 'ng-angular-popup';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../models/user.model';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrls: ['./create-registration.component.scss'],
+})
+export class CreateRegistrationComponent implements OnInit {
+  packages: string[] = ['Monthly', 'Quarterly', 'Yearly'];
+  genders: string[] = ['Male', 'Female'];
+  importantList: string[] = [
+    'Toxic fat reduction',
+    'Energy and endurance',
+    'Building Lean muscle',
+    'Healtier Digestive system',
+    'Sugar craving body',
+    'Fitness',
+  ];
+
+  registerForm!: FormGroup;
+  userIdToUpdate!: number;
+  isUpdateActive: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private api: ApiService,
+    private toastService: NgToastService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      mobile: [''],
+      weight: [''],
+      height: [''],
+      bmi: [''],
+      bmiResult: [''],
+      gender: [''],
+      requireTrainer: [''],
+      package: [''],
+      important: [''],
+      haveGymBefore: [''],
+      enquiryDate: [''],
+    });
+
+    this.registerForm.controls['height'].valueChanges.subscribe((res) => {
+      this.calculateBMI(res);
+    });
+
+    this.activatedRoute.params.subscribe((val) => {
+      this.userIdToUpdate = val['id'];
+      this.api.getRegisteredUserId(this.userIdToUpdate).subscribe((res) => {
+        this.isUpdateActive = true;
+        this.fillFormToUpdate(res);
+      });
+    });
+  }
+
+  submit() {
+    this.api.postRegisteration(this.registerForm.value).subscribe((res) => {
+      this.toastService.success({
+        detail: 'Success',
+        summary: 'Enquiry Added',
+        duration: 3000,
+      });
+      this.registerForm.reset();
+    });
+  }
+
+  update() {
+    this.api
+      .updateRegisterUser(this.registerForm.value, this.userIdToUpdate)
+      .subscribe((res) => {
+        this.toastService.success({
+          detail: 'Success',
+          summary: 'Update Successful',
+          duration: 3000,
+        });
+        this.registerForm.reset();
+        this.router.navigate(['list']);
+      });
+  }
+
+  calculateBMI(heightValue: number) {
+    const weight = this.registerForm.value.weight;
+    const height = heightValue;
+    const bmi = weight / height ** 2;
+    this.registerForm.controls['bmi'].patchValue(bmi);
+
+    switch (true) {
+      case bmi < 18.5:
+        this.registerForm.controls['bmiResult'].patchValue('UnderWeight');
+        break;
+
+      case bmi >= 18.5 && bmi < 25:
+        this.registerForm.controls['bmiResult'].patchValue('Normal');
+        break;
+
+      case bmi >= 25 && bmi < 30:
+        this.registerForm.controls['bmiResult'].patchValue('OverWeight');
+        break;
+
+      default:
+        this.registerForm.controls['bmiResult'].patchValue('Obese');
+        break;
+    }
+  }
+
+  fillFormToUpdate(user: User) {
+    this.registerForm.setValue({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+      weight: user.weight,
+      height: user.height,
+      bmi: user.bmi,
+      bmiResult: user.bmiResult,
+      gender: user.gender,
+      requireTrainer: user.requireTrainer,
+      package: user.package,
+      important: user.important,
+      haveGymBefore: user.haveGymBefore,
+      enquiryDate: user.enquiryDate,
+    });
+  }
+}
---
+++ src/app/user-detail/user-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
+})
+export class UserDetailComponent implements OnInit {
+
+  userId!: number
+  userDetail!: User
+
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(val => {
+      this.userId = val['id'];
+      this.fetchUserDetails(this.userId);
+    })
+  }
+
+  fetchUserDetails(userID: number) {
+    this.api.getRegisteredUserId(userID).subscribe(res => {
+      this.userDetail = res
+    })
+  }
+
+}
